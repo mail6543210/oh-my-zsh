13b946f55752192667e1f3e65816d7c212e268fb
mail6543210 <mail6543210@yahoo.com.tw>
2015-08-04 17:09:40 +0800

    Add system sources.list as well

--- custom/plugins/apt/apt.plugin.zsh
+++ custom/plugins/apt/apt.plugin.zsh
@@ -1,5 +1,5 @@
 function updaterepo() {
     for source in "$@"; do
-        sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/${source}" -o Dir::Etc::sourceparts="-" --no-list-cleanup
+        sudo apt-get update -o Dir::Etc::sourcelist="${source}" -o Dir::Etc::sourceparts="-" --no-list-cleanup
     done
 }
